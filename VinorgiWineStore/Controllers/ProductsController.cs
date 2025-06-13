8bf7c631abdc19644272583ca79e849685e3b069
Datn7 <datunashvili72@gmail.com>
2025-06-13 16:47:53 +0400

    add view and display products from database

---
+++ VinorgiWineStore/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinorgiWineStore.Services;
+
+namespace VinorgiWineStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
